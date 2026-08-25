e882c3e4220317704020e203c4382f06e1c6ad34
maleyka2908 <13584@holbertonstudents.com>
2026-08-25 08:38:46 +0000

    Fix 2-sort.rb output format

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/env ruby
 
-# Collect all arguments
 results = ARGV.map { |arg| arg }
 
-# Sort arguments: integers numerically, others lexicographically
 sorted_results = results.sort do |a, b|
-  # Check if both can be converted to integers
   a_is_int = a =~ /\A-?\d+\z/
   b_is_int = b =~ /\A-?\d+\z/
 
@@ -20,6 +17,4 @@ sorted_results = results.sort do |a, b|
   end
 end
 
-sorted_results.each do |result|
-  puts result
-end
+puts sorted_results.join(' ')
