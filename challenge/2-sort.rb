c4b0d158ea078ce79d9ae70444fbcf7c97f3f2b5
maleyka2908 <13584@holbertonstudents.com>
2026-08-27 06:53:58 +0000

    Fix sorting logic in 2-sort.rb to correctly order and print each integer on a new line

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,20 +1,11 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
-results = ARGV.map { |arg| arg }
-
-sorted_results = results.sort do |a, b|
-  a_is_int = a =~ /\A-?\d+\z/
-  b_is_int = b =~ /\A-?\d+\z/
-
-  if a_is_int && b_is_int
-    a.to_i <=> b.to_i
-  elsif a_is_int
-    -1
-  elsif b_is_int
-    1
-  else
-    a <=> b
-  end
+result = []
+ARGV.each do |arg|
+  next if arg !~ /^-?[0-9]+$/
+  result << arg.to_i
 end
 
-puts sorted_results.join(' ')
+result.sort.each do |n|
+  puts n
+end
